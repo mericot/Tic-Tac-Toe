5f45e79cdbb0abe6b9919a20ea3f17e21eedf3a2
Samuel Pimentel <mericot@Samuels-MacBook-Air.local>
2024-11-12 17:06:47 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/*
+Student Name: Samuel Pimentel 
+Student ID:00334334
+Course:N/A
+NOTES:Tic-Tac-Toe Project
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Tic_Tac_Toe tic = new Tic_Tac_Toe();
+
+    }
+}
